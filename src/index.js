d3577c99d9b111af9ac9c2ce9d079da82a8d874d
Vasil <vasilignatow@gmail.com>
2023-09-11 14:01:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 const express = require('express');
-const dbConfig = require('./config/mongoose.config');
 const config = require('./config/config')[process.env.NODE_ENV];
 const socketio = require('socket.io');
 
 const app = express();
-// const server = http.createServer(app);
+
 // Setup express server
 require('./config/express.config')(app);
 
-dbConfig(config.MONGODB_URL)
-    .then(() => {
-        console.log('DB connected!');
-        const server = app.listen(config.PORT, () => console.log('Server is listening on port: ' + config.PORT));
-        const io = socketio(server);
-        app.set('io', io);
-    })
-    .catch(err => {
-        console.log('DB connection error: ', err);
-    });
+const server = app.listen(config.PORT, () => console.log('Server is listening on port: ' + config.PORT));
+global.io = socketio(server, {
+    cors: {
+        origin: ['http://localhost:3001']
+    }
+});
+
+global.io.on('connection', (socket) => {
+    console.log(socket.id, 'is connected!');
+
+
+    socket.emit('new-event', { text: 'Welcome from server' })
+});
