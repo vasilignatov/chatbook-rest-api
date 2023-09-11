83451b1a94618803f2e5c0ddd14d23cf8ebdb6f5
Vasil <vasilignatow@gmail.com>
2023-09-11 14:00:48 +0300

    Update mongoose config

--- src/config/mongoose.config.js
+++ src/config/mongoose.config.js
@@ -1,8 +1,27 @@
 const mongoose = require('mongoose');
+const config = require('./config')[process.env.NODE_ENV];
 
-function dbConfig(connectionString) {
+function dbConnect() {
     mongoose.set('strictQuery', false);
-    return mongoose.connect(connectionString);
+
+    mongoose.connect(config.MONGODB_URL);
+
+    mongoose.connection.on('connected', () => {
+        console.log('Mongo has connected succesfully')
+    });
+
+    mongoose.connection.on('reconnected', () => {
+        console.log('Mongo has reconnected')
+    });
+
+    mongoose.connection.on('error', error => {
+        console.log('Mongo connection has an error', error)
+        mongoose.disconnect();
+    });
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Mongo connection is disconnected')
+    });
 }
 
-module.exports = dbConfig;
+module.exports = dbConnect;
