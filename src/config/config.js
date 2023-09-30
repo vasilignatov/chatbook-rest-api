38496ab87267f49d86c32f51d50688848d939b57
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-09-30 09:43:30 +0300

    Added imagekit config keys

--- src/config/config.js
+++ src/config/config.js
@@ -10,15 +10,19 @@ module.exports = {
         JWT_SECRET: '88819a11e0d24153de4fbe9c0b9d3206ea3ac16905bd6b44b7b6d11fc0863751a1ca4467ade1ff42fce5d0ecd7474d2913209127ccbc09356fde662b91f40360',
         SALT_ROUNDS: 1,
         JWT_ACCESS_EXPIRATION_MINUTES: 30,
-        JWT_REFRESH_EXPIRATION_DAYS: 30
+        JWT_REFRESH_EXPIRATION_DAYS: 30,
+        IMAGE_KIT_PUBLIC_KEY: 'public_BrrOwcySJsmMwiYPn7AABrM8mW8=',
+        IMAGE_KIT_PRIVATE_KEY: 'private_OmFmuN+//tGgoRoYri2GfBI7gfI='
     },
     production: {
         PORT: process.env.PORT,
         MONGODB_URL: process.env.MONGODB_URL,
-        JWT_SECRET: process.env.JWT_SECRET,
         SALT_ROUNDS: process.env.SALT_ROUNDS,
+        JWT_SECRET: process.env.JWT_SECRET,
         JWT_ACCESS_EXPIRATION_MINUTES: process.env.JWT_ACCESS_EXPIRATION_MINUTES,
-        JWT_REFRESH_EXPIRATION_DAYS: process.env.JWT_REFRESH_EXPIRATION_DAYS
+        JWT_REFRESH_EXPIRATION_DAYS: process.env.JWT_REFRESH_EXPIRATION_DAYS,
+        IMAGE_KIT_PUBLIC_KEY: process.env.IMAGE_KIT_PUBLIC_KEY,
+        IMAGE_KIT_PRIVATE_KEY:process.env.IMAGE_KIT_PRIVATE_KEY 
     },
     env: process.env.NODE_ENV
 }
