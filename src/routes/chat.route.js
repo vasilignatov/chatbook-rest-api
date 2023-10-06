b2f866ffb3e596fc783d5fcc0267a891ccd5b4fc
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-10-06 23:29:52 +0300

    Remove post message route

--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -4,7 +4,5 @@ const chatController = require('../controllers/chat.controller');
 router.get('/', chatController.getRecentChat);
 router.get('/:roomId', chatController.getChatByRoomId);
 router.post('/initiate', chatController.initiate);
-router.post('/:roomId', chatController.postMessage);
-
 
 module.exports = router;
